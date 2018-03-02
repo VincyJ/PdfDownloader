7ed5cefe73aa3377cd1b56d8909b5c674c1b0539
Vincy <arputhavincyj@gmail.com>
2018-03-02 15:09:37 +0530

    Initial commit

---
+++ app/src/main/java/myportfolio/com/pdfdownloader/MainActivity.java
@@ -0,0 +1,34 @@
+package myportfolio.com.pdfdownloader;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+
+        progressbar = (ProgressBar) findViewById(R.id.progressbar);
+        webview.getSettings().setJavaScriptEnabled(true);
+        String filename = "http://www.africau.edu/images/default/sample.pdf";
+        webview.loadUrl("http://docs.google.com/gview?embedded=true&url=" + filename);
+
+        webview.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                // do your stuff here
+                progressbar.setVisibility(View.GONE);
+            }
+        });
+    }
+}
